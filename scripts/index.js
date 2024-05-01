bc95dce7d311d5bedd10c05d16fc91167c52fe73
Jerry Liu <ljliu89@gmail.com>
2024-04-30 17:10:20 -0700

    project 5 final stage

--- scripts/index.js
+++ scripts/index.js
@@ -31,22 +31,52 @@ function getCardElement(data) {
   const cardElement = cardTemplete
     .querySelector(".elements__cards")
     .cloneNode(true);
+
+  const likeButton = cardElement.querySelector(".elements__like-button");
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("elements__like-button_active");
+  });
+
+  const deleteButton = cardElement.querySelector(".elements__delete-button");
+  deleteButton.addEventListener("click", () => {
+    cardElement.remove();
+  });
+
   const cardImage = cardElement.querySelector(".elements__image");
+  const cardTitle = cardElement.querySelector(".elements__title");
   cardImage.src = data.link;
   cardImage.alt = data.name;
-  cardElement.querySelector(".elements__title").textContent = data.name;
+  cardTitle.textContent = data.name;
+
+  const pictureModal = document.querySelector("#modal__picture");
+  const modalImage = document.querySelector(".modal__picture_large");
+  const modalImageTitle = document.querySelector(".modal__picture_title");
+  const pictureCloseButton = document.querySelector("#modal__picture_close");
+
+  cardImage.addEventListener("click", () => {
+    modalImage.src = cardImage.src;
+    modalImageTitle.textContent = cardTitle.textContent;
+    pictureModal.classList.add("modal_opened");
+  });
+
+  pictureCloseButton.addEventListener("click", () => {
+    pictureModal.classList.remove("modal_opened");
+  });
+
   return cardElement;
 }
 
 const cardContainer = document.querySelector(".elements__container");
 
-for (let i = 0; i < initialCards.length; i++) {
-  const cardElements = getCardElement(initialCards[i]);
-  cardContainer.append(cardElements);
-}
+initialCards.forEach((card) => {
+  cardContainer.append(getCardElement(card));
+});
 
 const editButton = document.querySelector(".profile__edit-button");
-const modal = document.querySelector(".modal");
+const editModal = document.querySelector("#modal__edit");
+
+const addButton = document.querySelector(".profile__add-button");
+const addModal = document.querySelector("#modal__add");
 
 const profileName = document.querySelector(".profile__title");
 const profileJob = document.querySelector(".profile__subtitle");
@@ -54,21 +84,32 @@ const profileJob = document.querySelector(".profile__subtitle");
 const inputName = document.querySelector(".modal__input_name");
 const inputJob = document.querySelector(".modal__input_job");
 
+const inputTitle = document.querySelector(".modal__input_title");
+const inputLink = document.querySelector(".modal__input_link");
+
 function clickEditButton() {
   inputName.value = profileName.textContent;
   inputJob.value = profileJob.textContent;
-  modal.classList.add("modal_opened");
+  editModal.classList.add("modal_opened");
+}
+
+function clickAddButton() {
+  addModal.classList.add("modal_opened");
 }
 
 editButton.addEventListener("click", clickEditButton);
+addButton.addEventListener("click", clickAddButton);
 
-const closeButton = document.querySelector(".modal__close");
+const editCloseButton = document.querySelector("#modal__edit_close");
+const addCloseButton = document.querySelector("#modal__add_close");
 
 function closeModal() {
-  modal.classList.remove("modal_opened");
+  editModal.classList.remove("modal_opened");
+  addModal.classList.remove("modal_opened");
 }
 
-closeButton.addEventListener("click", closeModal);
+editCloseButton.addEventListener("click", closeModal);
+addCloseButton.addEventListener("click", closeModal);
 
 const formElement = document.forms["profile-form"];
 
@@ -80,3 +121,16 @@ function saveForm(evt) {
 }
 
 formElement.addEventListener("submit", saveForm);
+
+const newCard = document.forms["new-card"];
+
+function createNewCard(evt) {
+  evt.preventDefault();
+  const cardContent = {};
+  cardContent.name = inputTitle.value;
+  cardContent.link = inputLink.value;
+  cardContainer.prepend(getCardElement(cardContent));
+  closeModal();
+}
+
+newCard.addEventListener("submit", createNewCard);
